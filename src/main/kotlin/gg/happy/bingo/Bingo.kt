c1add419cb3dd3c49ba610951073404594b8d72f
Happy <114749637+OoHappyoO@users.noreply.github.com>
2026-07-24 09:12:29 +0800

    Add resources

--- src/main/kotlin/gg/happy/bingo/Bingo.kt
+++ src/main/kotlin/gg/happy/bingo/Bingo.kt
@@ -2,8 +2,14 @@ package gg.happy.bingo
 
 import taboolib.common.platform.Plugin
 import taboolib.common.platform.function.info
+import taboolib.module.configuration.Config
+import taboolib.module.configuration.ConfigFile
 
 object Bingo : Plugin() {
+
+    @Config("config.yml", autoReload = true, migrate = true)
+    lateinit var conf: ConfigFile
+        private set
 
     override fun onEnable() {
         info("Successfully running Bingo!")
